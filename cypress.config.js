b07d0a3f345dfcbfee9a59fe1df4d78f481f6c4a
Matheus Dos Santos <matheus2050@gmail.com>
2024-12-08 19:56:36 -0300

    fix: ajuste no arquivo cypress.config.js para executar os testes de arquivos .feature

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,20 @@
-const { defineConfig } = require("cypress");
-const createEsbuildPlugin = require("@bahmutov/cypress-esbuild-preprocessor");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      const bundler = createEsbuildPlugin(config);
-      addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor", bundler);
-      return config;
+    e2e: {
+        setupNodeEvents(on, config) {
+            const bundler = createBundler({
+                plugins: [createEsbuildPlugin(config)],
+            });
+
+            on("file:preprocessor", bundler);
+            addCucumberPreprocessorPlugin(on, config);
+            return config;
+
+        },
+        specPattern: "cypress/e2e/features/*.feature",
     },
-    specPattern: "cypress/e2e/**/*.feature",
-  },
 });
