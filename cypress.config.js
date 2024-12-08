1d7edbdb7513b994b0aef0c06a511d9d958f564f
Matheus Dos Santos <matheus2050@gmail.com>
2024-12-07 23:08:50 -0300

    feat: Configuração do cypress.config.js para ler os arquivos .feature

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
+const createEsbuildPlugin = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const bundler = createEsbuildPlugin(config);
+      addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", bundler);
+      return config;
     },
+    specPattern: "cypress/e2e/**/*.feature",
   },
 });
